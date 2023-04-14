3392c6021bcc4ca48131d93c1c1d71dce01b20fa
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-04-14 16:41:50 +0200

    conditional added to allow user to choose what propt pop up they get for signing in

--- frontend/src/Components/Landing.tsx
+++ frontend/src/Components/Landing.tsx
@@ -1,5 +1,8 @@
-import { useNavigate } from 'react-router-dom';
 import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import NewPlayer from './NewPlayer';
+import ExistingPlayer from './ExistingPlayer';
 
 interface LandingProps  {
   navigate: ReturnType<typeof useNavigate>;
@@ -7,11 +10,15 @@ interface LandingProps  {
 
 const Landing: React.FC<LandingProps> = ({ navigate }) => {
 
+  const [popup, setPopup] = useState(0);
+
   return (
     <div className="landing-container">
-    <h1>Welcome to the landing page</h1>
-    <button>Register new player</button>
-    <button>Use exisitng player</button>
+    <h1>Welcome to Super Scrabble!</h1>
+    <button onClick={() => setPopup(1)}>Register new player</button>
+    <button onClick={() => setPopup(2)}>Use exisitng player</button>
+    {popup === 1 && <NewPlayer />}
+    {popup === 2 && <ExistingPlayer />}
     </div>
   )
 }
--- frontend/src/Components/NewPlayer.tsx
+++ frontend/src/Components/NewPlayer.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import '../styling/NewPlayer.scss';
 
 const NewPlayer = () => {
 
   return (
-    <div>New player</div>
+    <div className="new-player-container">
+    <h2>New player</h2>
+    <label className="new-player-label">Please choose a username:</label>
+    <input className="new-player-username" type="text" />
+    <label className="new-player-label">Please choose a password:</label>
+    <input className="new-player-password" type="password" />
+    <label className="new-player-label">Please confirm your password:</label>
+    <input className="new-player-password" type="password" />
+    </div>
   )
 }
 
