fb573144dc1d7d0a6a79ce52b2dadc6d93b54369
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-04-14 17:42:27 +0200

    new player screen changes

--- frontend/src/Components/Landing.tsx
+++ frontend/src/Components/Landing.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NewPlayer from './NewPlayer';
 import ExistingPlayer from './ExistingPlayer';
+import '../styling/Landing.scss';
 
 interface LandingProps  {
   navigate: ReturnType<typeof useNavigate>;
@@ -10,15 +11,19 @@ interface LandingProps  {
 
 const Landing: React.FC<LandingProps> = ({ navigate }) => {
 
-  const [popup, setPopup] = useState(0);
+  const [popup, setPopup] = useState(false);
+
+  const handleNewPlayerPopup = () => {
+    setPopup(!popup);
+    document.body.classList.toggle('landing-container-gray');
+  }
 
   return (
-    <div className="landing-container">
+    <div>
     <h1>Welcome to Super Scrabble!</h1>
-    <button onClick={() => setPopup(1)}>Register new player</button>
-    <button onClick={() => setPopup(2)}>Use exisitng player</button>
-    {popup === 1 && <NewPlayer />}
-    {popup === 2 && <ExistingPlayer />}
+    <button onClick={handleNewPlayerPopup}>Register new player</button>
+    {/* <button onClick={() => setPopup(2)}>Use exisitng player</button> */}
+    {popup && <NewPlayer />}
     </div>
   )
 }
--- frontend/src/Components/NewPlayer.tsx
+++ frontend/src/Components/NewPlayer.tsx
@@ -6,12 +6,18 @@ const NewPlayer = () => {
   return (
     <div className="new-player-container">
     <h2>New player</h2>
+    <div className="line-container">
     <label className="new-player-label">Please choose a username:</label>
     <input className="new-player-username" type="text" />
+    </div>
+    <div className="line-container">
     <label className="new-player-label">Please choose a password:</label>
     <input className="new-player-password" type="password" />
+    </div>
+    <div className="line-container">
     <label className="new-player-label">Please confirm your password:</label>
     <input className="new-player-password" type="password" />
+    </div>
     </div>
   )
 }
