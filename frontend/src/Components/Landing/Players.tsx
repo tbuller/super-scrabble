fef1b94699b59a6d0940e5f2e01f6e35f7636fd3
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-04-15 10:56:41 +0200

    some styling

--- frontend/src/Components/Landing/Players.tsx
+++ frontend/src/Components/Landing/Players.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/usersSlice';
+import '../../styling/Players.scss';
 
 const Players = () => {
 
   const users = useSelector((state: RootState) => state.users.users);
 
   return (
-    <div>
+    <div className="player-icon-container">
     {users.map(u => 
-      <div>{u.username[0]}</div>
+      <div className="player-icon">{u.username[0]}</div>
     )}  
     </div>
   )
