47c30f601187ae0fa5c9748a202b3838f8090a9f
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-04-27 10:12:37 +0200

    port bug fix

--- frontend/src/Components/Landing/Players.tsx
+++ frontend/src/Components/Landing/Players.tsx
@@ -14,16 +14,22 @@ const Players = () => {
     dispatch(addCurrentPlayer(player));
   }
 
+  const showUsers = () => {
+    console.log(users);
+    console.log(currentPlayers);
+  }
+
   return (
     <div className="players-icons-container">
     {users.map(u =>
       <div className="player-icon-container" key={u._id}>
       <div className={`player-icon${currentPlayers.find(p => p._id === u._id) ? ' selected' : ''}`} key={u._id} onClick={() => addPlayer(u)}>
-        {u.username[0].toUpperCase()}
+        {u.username?.[0].toUpperCase()}
         <div className="popup">{u.username}</div>
       </div>
       </div>
-    )}  
+    )} 
+    <button onClick={showUsers}>bug fixing button</button> 
     </div>
   )
 }
