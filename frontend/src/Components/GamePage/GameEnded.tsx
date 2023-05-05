976266b7e39cc004261205e5bec5cc1fc98c6891
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-05-05 10:48:56 +0200

    some styling for the error page

--- frontend/src/Components/GamePage/GameEnded.tsx
+++ frontend/src/Components/GamePage/GameEnded.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootStateUsers } from '../../redux/usersSlice';
 
@@ -7,13 +7,19 @@ const GameEnded = () => {
 
   const currentPlayers = useSelector((state: RootStateUsers) => state.users.currentPlayers);
 
+  const [sortedResults, setSortedResults] = useState(currentPlayers);
+
   useEffect(() => {
-    console.log(currentPlayers);
+    const sorted = [...currentPlayers].sort((a, b) => b.currentScore - a.currentScore);
+    setSortedResults(sorted);
   }, [])
 
   return (
     <div className="ended-game-container">
-
+      <h1 className="ended-game-greeting">Game ended, thank you for playing</h1>
+      {sortedResults && sortedResults.map((player: any, index) =>
+        <div>#{index + 1}</div>  
+        )}  
     </div>
   )
 }
--- frontend/src/Components/Landing/Players.tsx
+++ frontend/src/Components/Landing/Players.tsx
@@ -30,7 +30,6 @@ const Players = () => {
       </div>
     )} 
     <button onClick={showUsers}>bug fixing button</button> 
-    <a href="https://github.com/tbuller/all-purpose-organiser">all-purpose-organiser</a>
     </div>
   )
 }
