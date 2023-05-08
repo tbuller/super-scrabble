96cde528bc59a8f659f9e3250ab251f94087727c
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-05-08 13:37:50 +0200

    some more stylig updates

--- frontend/src/Components/GamePage/ScoreBoard.tsx
+++ frontend/src/Components/GamePage/ScoreBoard.tsx
@@ -7,13 +7,14 @@ import '../../styling/ScoreBoard.scss';
 const ScoreBoard = () => {
 
   const currentPlayers = useSelector((state: RootStateUsers) => state.users.currentPlayers);
+  const currentTurn = useSelector((state: RootStateUsers) => state.users.currentTurn);
 
   return (
     <div className="score-board-container">
     {currentPlayers && currentPlayers.map(p =>
-      <div className="individual-player-container" key={p._id}>
-      <div>{p.username}</div>  
-      <div>{p.currentScore}</div>
+      <div className={`individual-player-container${p._id === currentTurn?._id ? " selected" : ""}`} key={p._id}>
+      <div className="score-board-username">{p.username}</div>  
+      <div className="score-board-current-score">{p.currentScore}</div>
       </div>
       )} 
     </div>
